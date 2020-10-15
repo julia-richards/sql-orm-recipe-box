1c6fdb3c5cb133a51a7d7816bfcb35a7ee8a800e
julia-richards <julia.m.richards@gmail.com>
2020-10-15 17:11:37 -0600

    finished phase 10

--- models/ingredient.js
+++ models/ingredient.js
@@ -1,15 +1,41 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Ingredient = sequelize.define('Ingredient', {
-    amount: DataTypes.NUMERIC,
-    measurementUnitId: DataTypes.INTEGER,
-    foodStuff: DataTypes.STRING,
-    recipeId: DataTypes.INTEGER
-  }, {});
-  Ingredient.associate = function(models) {
-    // associations can be defined here
-    Ingredient.belongsTo(models.Recipe, { foreignKey: 'recipeId'});
-    Ingredient.belongsTo(models.MeasurementUnit, { foreignKey: 'measurementUnitId'});
-  };
-  return Ingredient;
+	const Ingredient = sequelize.define(
+		"Ingredient",
+		{
+			amount: {
+				type: DataTypes.NUMERIC,
+				validate: {
+					notNull: true,
+				},
+			},
+			measurementUnitId: {
+				type: DataTypes.INTEGER,
+				validate: {
+					notNull: true,
+				},
+			},
+			foodStuff: {
+				type: DataTypes.STRING,
+				validate: {
+					notEmpty: true,
+				},
+			},
+			recipeId: {
+				type: DataTypes.INTEGER,
+				validate: {
+					notNull: true,
+				},
+			},
+		},
+		{}
+	);
+	Ingredient.associate = function (models) {
+		// associations can be defined here
+		Ingredient.belongsTo(models.Recipe, { foreignKey: "recipeId" });
+		Ingredient.belongsTo(models.MeasurementUnit, {
+			foreignKey: "measurementUnitId",
+		});
+	};
+	return Ingredient;
 };
